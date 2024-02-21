ea04a1ca3fe4ad43c253b4472304d19dae76807a
Beeawaks <alfarizgs1@gmail.com>
2024-02-22 01:32:10 +0700

    add details

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -13,7 +13,7 @@ import wind_icon from '../Assets/wind.png';
 
 const WeatherApp = () => {
 
-    let api_key = "YOUR_USER_API_KEY"
+    let api_key = "YOUR_API_KEY"
     const [wicon, setWicon] = useState(clear_icon);
 
     const search = async () => {
@@ -30,11 +30,22 @@ const WeatherApp = () => {
         const wind = document.getElementsByClassName("wind-rate");
         const temperature = document.getElementsByClassName("weather-temp");
         const location = document.getElementsByClassName("weather-location");
+        const type = document.getElementsByClassName("weather-type");
+        const feelsLike = document.getElementsByClassName("feels-like");
+        const pressure = document.getElementsByClassName("pressure-rate");
+        const tempMin = document.getElementsByClassName("temp-min");
+        const tempMax = document.getElementsByClassName("temp-max");
+
 
         humidity[0].innerHTML = data.main.humidity + " %";
         wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
         temperature[0].innerHTML = Math.floor(data.main.temp) + " °C";
         location[0].innerHTML = data.name;
+        type[0].innerHTML = data.weather[0].main;
+        feelsLike[0].innerHTML = Math.floor(data.main.feels_like) + " °C";
+        pressure[0].innerHTML = Math.floor(data.main.pressure) + " hPa";
+        tempMin[0].innerHTML = Math.floor(data.main.temp_min) + " °C";
+        tempMax[0].innerHTML = Math.floor(data.main.temp_max) + " °C";
 
         if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n"){
             setWicon(clear_icon);
@@ -62,6 +73,12 @@ const WeatherApp = () => {
         }
     }
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
   return (
     <div className="container">
         <div className="top-bar">
@@ -76,6 +93,7 @@ const WeatherApp = () => {
         <div className="weather-image">
             <img src={wicon} alt="" />
         </div>
+        <div className="weather-type">-</div>
         <div className="weather-temp">-</div>
         <div className="weather-location">-</div>
         <div className="data-container">
@@ -93,6 +111,72 @@ const WeatherApp = () => {
                     <div className="text">Wind Speed</div>
                 </div>
             </div>
+        </div>
+
+        <div className="detail-button">
+            {isOpen ? (
+                <button onClick={toggleDropdown} style={{
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    color: 'white',
+                    padding: '15px 32px',
+                    textAlign: 'center',
+                    textDecoration: 'none',
+                    display: 'inline-block',
+                    fontSize: '20px',
+                    margin: '4px 2px',
+                    cursor: 'pointer',
+                    borderRadius: '8px',
+                  }}>Hide Details</button>
+                ) : (
+                <button onClick={toggleDropdown} style={{
+                    backgroundColor: 'transparent', 
+                    border: 'none',
+                    color: 'white',
+                    padding: '15px 32px',
+                    textAlign: 'center',
+                    textDecoration: 'none',
+                    display: 'inline-block',
+                    fontSize: '20px',
+                    margin: '4px 2px',
+                    cursor: 'pointer',
+                    borderRadius: '8px',
+                  }}>Show Details</button>
+                )}
+            {isOpen && (
+                <>
+                <div className="detail-container">
+                    <div className="element">
+                        <div className="data">
+                            <div className="feels-like">-</div>
+                            <div className="text">Feels Like</div>
+                        </div>
+                    </div>
+                    <div className="element">
+                        <div className="data">
+                            <div className="pressure-rate">-</div>
+                            <div className="text">Pressure</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="detail-container">
+                    <div className="element">
+                        <div className="data">
+                            <div className="temp-min">-</div>
+                            <div className="text">Temperature (Min)</div>
+                        </div>
+                    </div>
+                    <div className="element">
+                        <div className="data">
+                            <div className="temp-max">-</div>
+                            <div className="text">Temperature (Max)</div>
+                        </div>
+                    </div>
+                </div>
+
+                </>
+            )}
         </div>
     </div>
   )
